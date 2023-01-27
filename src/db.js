8256755241c3b8686066959149415dce125d542c
JetBrain2056 <st@brainrs.com>
2023-01-27 09:27:22 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,     // Имя БД
-    process.env.DB_USER,     // Пользователь
-    process.env.DB_PASSWORD, // Пароль
+    process.env.DB_NAME,    
+    process.env.DB_USER,     
+    process.env.DB_PASSWORD, 
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,

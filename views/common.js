9e3df45554bc1704c12e5bdbfe00ff5a5f263e03
JetBrain2056 <st@brainrs.com>
2023-01-28 10:00:31 +0300

    Update common.js

--- views/common.js
+++ views/common.js
@@ -1,8 +1,59 @@
 let select_rows = [];
 
 const user_tbl = document.getElementById('user_table');
-user_tbl.addEventListener('click', row_select);   
+user_tbl.addEventListener('click', row_select); 
+  
+function row_select(e) {      
+  //console.log(e.path[1]);                
+  //console.log(e.target);
 
+  const path = e.path || (e.composedPath && e.composedPath());
+  const row  = path[1];
+  //console.log(row.cells[0].innerText);
+  //console.log(row.cells[0].nodeName );
+  
+  //if (row.cells[0].innerText === 'id') { return;
+  if (row.cells[0].nodeName  === "TH" ){ 
+    console.log(row.cells[0].parentNode.parentNode);
+    //for (const table of allTables) {
+      const tBody = user_tbl.tBodies[0];
+      const rows = Array.from(tBody.rows);
+      const headerCells = row.cells;
+    
+      for (const th of headerCells) {
+        const cellIndex = th.cellIndex;
+    
+        th.addEventListener("click", () => {
+          rows.sort((tr1, tr2) => {
+            const tr1Text = tr1.cells[cellIndex].textContent;
+            const tr2Text = tr2.cells[cellIndex].textContent;
+            return tr1Text.localeCompare(tr2Text);
+          });
+    
+          tBody.append(...rows);
+        });
+      }
+    //return;
+  }else{
+      let text;
+      if (e.ctrlKey) {
+          text = "The CTRL key was pressed!";
+          select_rows.push(row);
+          row.style.background = "aquamarine";
+          console.log("select_rows count: ", select_rows.length);
+      } else {
+          text = "The CTRL key was NOT pressed!";
+          for (const rows of select_rows) {
+              rows.style.background = "";
+          }
+          select_rows.splice(0, select_rows.length);
+          row.style.background = "aquamarine";
+          select_rows.push(row);
+      }
+      console.log(text);
+  }
+}
+/////////////////////////////////////////////////////////////////////////////
 async function user_table() {
   //const response = await fetch('http://192.168.1.8:3000/users');
   //const data = await response.json();
@@ -89,10 +140,8 @@ async function user_create()  {
   })  
   .catch(error => {
     console.log('Request failed', error);
-  });      
-  
+  });        
 }
-
 async function user_delete() {
 
   for (const row of select_rows){
@@ -118,56 +167,5 @@ async function user_delete() {
       });          
   }   
   user_table();
-}
-
-function row_select(e) {      
-  //console.log(e.path[1]);                
-  //console.log(e.target);
-
-  const path = e.path || (e.composedPath && e.composedPath());
-  const row  = path[1];
-  //console.log(row.cells[0].innerText);
-  //console.log(row.cells[0].nodeName );
-  
-  //if (row.cells[0].innerText === 'id') { return;
-  if (row.cells[0].nodeName  === "TH" ){ 
-    console.log(row.cells[0].parentNode.parentNode);
-    //for (const table of allTables) {
-      const tBody = user_tbl.tBodies[0];
-      const rows = Array.from(tBody.rows);
-      const headerCells = row.cells;
-    
-      for (const th of headerCells) {
-        const cellIndex = th.cellIndex;
-    
-        th.addEventListener("click", () => {
-          rows.sort((tr1, tr2) => {
-            const tr1Text = tr1.cells[cellIndex].textContent;
-            const tr2Text = tr2.cells[cellIndex].textContent;
-            return tr1Text.localeCompare(tr2Text);
-          });
-    
-          tBody.append(...rows);
-        });
-      }
-    //return;
-  }else{
-      let text;
-      if (e.ctrlKey) {
-          text = "The CTRL key was pressed!";
-          select_rows.push(row);
-          row.style.background = "aquamarine";
-          console.log("select_rows count: ", select_rows.length);
-      } else {
-          text = "The CTRL key was NOT pressed!";
-          for (const rows of select_rows) {
-              rows.style.background = "";
-          }
-          select_rows.splice(0, select_rows.length);
-          row.style.background = "aquamarine";
-          select_rows.push(row);
-      }
-      console.log(text);
-  }
 }
 user_table();

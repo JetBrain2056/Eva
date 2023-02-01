9362954491399d933c63aa9035b3081c68c968e2
JetBrain2056 <st@brainrs.com>
2023-02-01 19:18:29 +0300

    Update aside.js

--- views/template/common/aside.js
+++ views/template/common/aside.js
@@ -1,44 +1,5 @@
-const asideList = [
-    {
-        href: '#',
-        title: 'Подсистемы',
-    },
-    {
-        href: '#',
-        title: 'Справочники',
-    },
-    {
-        href: '#',
-        title: 'Константы',
-    },
-    {
-        href: '#',
-        title: 'Документы',
-    },
-    {
-        href: '#',
-        title: 'Обработки',
-    },
-    {
-        href: '#',
-        title: 'Регистры',
-    },
-    {
-        href: '#',
-        title: 'Модули',
-    },
-    {
-        href: '#',
-        title: 'Отчеты',
-    },
-    {
-        href: '#',
-        title: 'Роли',
-    },
-]
+//const columnLeft = document.querySelector('.columnLeft')
 
-const columnLeft = document.querySelector('.columnLeft')
-
-columnLeft.innerHTML = asideList.map(link => (
-    `<li class="nav-item"><a class="nav-link btn" href="${link.href}">${link.title}</a></li>`
-)).join('')
+// columnLeft.innerHTML = asideList.map(link => (
+//     `<li class="nav-item"><a class="nav-link btn" href="${link.href}">${link.title}</a></li>`
+// )).join('')

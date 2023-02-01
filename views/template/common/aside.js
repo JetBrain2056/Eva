82562a4aa556ea7a3cf46e9ddfe86773210f6be0
JetBrain2056 <st@brainrs.com>
2023-02-01 20:13:07 +0300

    Delete aside.js

--- views/template/common/aside.js
+++
@@ -1,5 +0,0 @@
-//const columnLeft = document.querySelector('.columnLeft')
-
-// columnLeft.innerHTML = asideList.map(link => (
-//     `<li class="nav-item"><a class="nav-link btn" href="${link.href}">${link.title}</a></li>`
-// )).join('')

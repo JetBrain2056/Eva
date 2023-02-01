42d3e43aabe475f5c6528e6554dbde65cfc1fe75
JetBrain2056 <st@brainrs.com>
2023-02-01 12:51:21 +0300

    Update common.js

--- views/common.js
+++ views/common.js
@@ -4,19 +4,13 @@ const user_tbl = document.getElementById('user_table');
 if (user_tbl) {user_tbl.addEventListener('click', row_select)}
 
 function row_select(e) {      
-  console.log("row_select"); 
-  //console.log(e.path[1]);                
-  //console.log(e.target);
+  console.log('row_select'); 
 
   const path = e.path || (e.composedPath && e.composedPath());
   const row  = path[1];
-  //console.log(row.cells[0].innerText);
-  //console.log(row.cells[0].nodeName );
-  
-  //if (row.cells[0].innerText === 'id') { return;
-  if (row.cells[0].nodeName  === "TH" ){ 
-    console.log(row.cells[0].parentNode.parentNode);
-    //for (const table of allTables) {
+    
+  if (row.cells[0].nodeName  === 'TH') { 
+      console.log(row.cells[0].parentNode.parentNode);    
       const tBody = user_tbl.tBodies[0];
       const rows = Array.from(tBody.rows);
       const headerCells = row.cells;
@@ -24,7 +18,7 @@ function row_select(e) {
       for (const th of headerCells) {
         const cellIndex = th.cellIndex;
     
-        th.addEventListener("click", () => {
+        th.addEventListener('click', () => {
           rows.sort((tr1, tr2) => {
             const tr1Text = tr1.cells[cellIndex].textContent;
             const tr2Text = tr2.cells[cellIndex].textContent;
@@ -33,31 +27,30 @@ function row_select(e) {
     
           tBody.append(...rows);
         });
-      }
-    //return;
-  }else{
+      }    
+  } else {
       let text;
       if (e.ctrlKey) {
-          text = "The CTRL key was pressed!";
+          text = 'The CTRL key was pressed!';
           select_rows.push(row);
-          row.style.background = "aquamarine";
-          console.log("select_rows count: ", select_rows.length);
+          row.style.background = 'aquamarine';
+          console.log('select_rows count: ', select_rows.length);
       } else {
-          text = "The CTRL key was NOT pressed!";
+          text = 'The CTRL key was NOT pressed!';
           for (const rows of select_rows) {
-              rows.style.background = "";
+              rows.style.background = '';
           }
           select_rows.splice(0, select_rows.length);
-          row.style.background = "aquamarine";
+          row.style.background = 'aquamarine';
           select_rows.push(row);
       }
       console.log(text);
   }
 }
 async function show_user_table() {
-  console.log("show_user_table"); 
+  console.log('show_user_table'); 
   let data = await getUsers();
-  user_tbl.innerHTML = "";
+  user_tbl.innerHTML = '';
 
   const thead = document.createElement('thead');
   thead.style.border = '#00ff92';
@@ -79,9 +72,10 @@ async function show_user_table() {
   //  while (i < 30) {     
   //   data.push({'id':'.'}); 
   //   i++;
-  //  }      
+  //  } 
+
   for (const rows of data) {              
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr');
     tbody.appendChild(tr);                
     const p = {id:rows.id, Name:rows.Name, Descr:rows.Descr};
     for (const element of Object.keys(p) ) {                                 
@@ -94,7 +88,7 @@ async function show_user_table() {
 /////////////////////////////////////////////////////////////////////////////
 
 async function getUsers() {
-  console.log("getUsers"); 
+  console.log('getUsers'); 
   let res;
   try{
     const response = await fetch('/users'); 
@@ -105,17 +99,17 @@ async function getUsers() {
   return res;
 }
 async function create_user(user) {
-  console.log("create_user", user); 
+  console.log('create_user', user); 
   let res;
   try {
     let response = await fetch('/create', { 
       method  : 'post',    
-      headers: {"Content-Type": "application/json"},
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(user)            
     });
     res = await response.json();
-  } catch(e) {
-    console.log(e);
+  } catch(err) {
+    console.log(err);
   }
   return res;
 }   
@@ -159,17 +153,17 @@ async function user_create() {
   }
 }
 async function delete_user(user){
-  console.log("delete_user", user);
+  console.log('delete_user', user);
   let res;
   try {
     let response = await fetch('/delete', { 
       method  : 'post',        
-      headers: {"Content-Type": "application/json"},
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(user)            
     });
     res = await response.json();
-  } catch (ex) {
-    console.log(ex);
+  } catch (err) {
+    console.log(err);
   }
   return res;
 }

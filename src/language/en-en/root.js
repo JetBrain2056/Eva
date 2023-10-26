5c2aa6fa6b2289556031c56cb9b134377d895557
JetBrain2056 <st@brainrs.com>
2023-10-26 18:29:18 +0300

    Create root.js

---
+++ src/language/en-en/root.js
@@ -0,0 +1,57 @@
+let root = {
+    // Locale
+    $_code				    : 'en',
+    $_direction				: 'ltr',
+    $_date_format_short	    : 'd.m.Y',
+    $_date_format_long		: 'l, d F Y',
+    $_time_format			: 'H:i:s',
+    $_datetime_format		: 'd/m/Y H:i:s',
+    $_decimal_point			: '.',
+    $_thousand_point		: '',
+
+    // Text
+    $_text_home				: '<i class:"fa fa-home"></i>',
+    $_text_yes				: 'Yes',
+    $_text_no				: 'No',
+    $_text_none				: ' --- Not select --- ',
+    $_text_select			: ' --- Selected --- ',
+    $_text_all_zones		: 'All zone',
+    $_text_pagination		: 'Views on %d end %d of %d (total %d pages)',
+    $_text_loading			: 'Upload...',
+    $_text_no_results		: 'No data!',
+
+    // Buttons
+    $_button_back					: 'Back',
+    $_button_continue				: 'Continue',
+    $_button_cancel					: 'Cancel',
+    $_button_close					: 'Close',
+    $_button_open 					: 'Open',
+    $_button_compare				: 'Compare',
+    $_button_wishlist				: 'Wishlist',
+    $_button_check    				: 'Check',
+    $_button_confirm				: 'Confirm',
+    $_button_delete					: 'Delete',
+    $_button_download				: 'Download',
+    $_button_edit			    	: 'Edit',
+    $_button_filter					: 'Filter',
+    $_button_write					: 'Write',
+    $_button_save 					: 'Save',
+    $_button_login					: 'Login',
+    $_button_update					: 'Update',
+    $_button_add  					: 'Add',
+    $_button_return					: 'Return',
+    $_button_search					: 'Search',
+    $_button_submit					: 'Submit',
+    $_button_view					: 'View',
+    $_button_upload					: 'Upload file',
+    $_button_list			    	: 'List',
+    $_button_grid					: 'Grid',    
+    $_button_copy                   : 'Copy',
+    $_button_etc                    : 'etc',
+    
+
+    // Error
+    $_error_exception       : 'Error code(%s): %s in %s on string %s'
+}
+
+module.exports = { root }

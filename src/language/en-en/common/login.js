3a06eb41d62b9af04145f1d9c82661e43880a4a6
JetBrain2056 <st@brainrs.com>
2023-10-27 09:31:11 +0300

    Create login.js

---
+++ src/language/en-en/common/login.js
@@ -0,0 +1,22 @@
+let login = {
+    // Heading
+    $_heading_title  : 'Authorization',
+
+    // Text
+    $_text_heading   : 'Authorization',
+    $_text_login     : 'Log in to the system EVA',
+    $_text_forgotten : 'Forgotten?',
+
+    // Entry
+    $_entry_username : 'Username',
+    $_entry_password : 'Password',
+
+    // Button
+    $_button_login   : 'Login',
+
+    // Error
+    $_error_login    : 'Such login and/or password does not exist!',
+    $_error_token    : 'Invalid token session. Log in again.'
+}
+
+module.exports =   { login }

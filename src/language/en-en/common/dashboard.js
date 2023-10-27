06f27a68d018054dabf5d6db440a7779dc0042d7
JetBrain2056 <st@brainrs.com>
2023-10-27 09:30:16 +0300

    Create dashboard.js

---
+++ src/language/en-en/common/dashboard.js
@@ -0,0 +1,12 @@
+let dashboard = {
+    // Heading
+    $_heading_title : 'Панель состояния',
+
+    // Error
+    $_error_install : 'ВНИМАНИЕ: Установочная папка до сих пор существует! Удалите папку install',
+
+    //modal
+    $_text_addusers : 'Добавить пользователя'
+}
+
+module.exports =   { dashboard }

d5c6ef90edc35073dba5eab6565c2e4ba729bc78
JetBrain2056 <st@brainrs.com>
2023-10-27 09:31:08 +0300

    Create header.js

---
+++ src/language/en-en/common/header.js
@@ -0,0 +1,19 @@
+let header = {
+    // Heading
+    $_heading_title : 'Administration',
+
+    // Text
+    $_text_profile  : 'Profile',
+    $_text_help     : 'Help',
+    $_text_homepage : 'Home page ',
+    $_text_support  : 'Support',
+    $_text_documentation : 'Documentation',
+    $_text_logout   : 'Logout',
+    $_text_config   : 'Configuration',
+    $_text_administration : 'Administration',
+    $_about_program : 'About program...',
+    $_text_roles    : 'Roles',
+    $_text_log      : 'Log'
+}
+
+module.exports =  { header }

910464a019a850fa730b98113b04f051cbde0cea
JetBrain2056 <st@brainrs.com>
2023-10-29 08:08:17 +0300

    Update dashboard.js

--- src/language/ru-ru/common/dashboard.js
+++ src/language/ru-ru/common/dashboard.js
@@ -3,12 +3,18 @@ let dashboard = {
     $_heading_title : 'Панель состояния',
 
     // Error
-    $_error_install : 'ВНИМАНИЕ: Установочная папка до сих пор существует! Удалите папку install',
+    $_error_install : 'ВНИМАНИЕ: ошибка установки приложения!',
 
     //modal
-    $_text_addusers : 'Добавить пользователя',
-    $_text_constant : 'Константа',
-    $_text_reference : 'Справочник'
+    $_text_addusers  : 'Добавить пользователя',
+    $_text_subsystem : 'Подсистема',
+    $_text_module    : 'Модуль',
+    $_text_constant  : 'Константа',
+    $_text_reference : 'Справочник',
+    $_text_document  : 'Документ',
+    $_text_processing : 'Обработка',
+    $_text_register  : 'Регистр',
+    $_text_report    : 'Отчет'
 }
 
 module.exports =   { dashboard }

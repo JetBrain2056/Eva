54fcb5ee881cd817074c699584012a54007be20d
JetBrain2056 <st@brainrs.com>
2023-10-29 08:08:13 +0300

    Update dashboard.js

--- src/language/en-en/common/dashboard.js
+++ src/language/en-en/common/dashboard.js
@@ -6,9 +6,15 @@ let dashboard = {
     $_error_install : 'Error: install',
 
     //modal
-    $_text_add_user : 'Add user',
-    $_text_constant : 'Constant',
-    $_text_reference : 'Reference'
+    $_text_add_user  : 'Add user',
+    $_text_subsystem : 'Subsystem',
+    $_text_module    : 'Module',
+    $_text_constant  : 'Constant',
+    $_text_reference : 'Reference',
+    $_text_document  : 'Document',
+    $_text_processing : 'Processing',
+    $_text_register  : 'Register',
+    $_text_report    : 'Report'
 }
 
-module.exports =   { dashboard }
+module.exports = { dashboard }

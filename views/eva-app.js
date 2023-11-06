adc3e8bc5d3f6dbdab37f52f85ecf844a32f12f1
JetBrain2056 <st@brainrs.com>
2023-11-06 07:11:47 +0300

    Update eva-app.js

--- views/eva-app.js
+++ views/eva-app.js
@@ -1,10 +1,39 @@
 
+async function getSubsystems() {
+    console.log('>>getSubsystems()...');
+    let res;
+    try{
+        const response = await fetch('/subsystems');
+        res = await response.json();
+    } catch (err) {
+        console.log(err)
+    }
+    return res;  
+}
+async function header(navTab) {
+    
+    let data = await getSubsystems();
+    console.log(data);
+    for (let row of data) {
+        console.log(row.name);
+        const li = document.createElement('li');
+        li.setAttribute("class","nav-item");
+        const a = document.createElement('a');
+        a.setAttribute("class","nav-link active");        
+        a.innerText = row.name;
+        li.appendChild(a); 
+        navTab.appendChild(li); 
+    }
+}
 function init() {
     const mode = document.querySelector('.content').getAttribute('data-mode');
     console.log('mode: ' + mode);   
     if (mode==='false') {
         const app = document.getElementById('eva-app');
-        console.log(app);       
+        console.log(app); 
+        
+        const navTab = document.getElementById("eva-gen");
+        header(navTab);
         const div = document.createElement('div');
         console.log(div);
         div.setAttribute("class", "d-flex flex-row flex-grow-1");              

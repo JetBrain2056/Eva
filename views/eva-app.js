c49a946b6c344faaffb1c796424506e9106f4c19
JetBrain2056 <st@brainrs.com>
2023-11-07 09:24:59 +0300

    Update eva-app.js

--- views/eva-app.js
+++ views/eva-app.js
@@ -25,16 +25,17 @@ async function getConfig() {
 function openRef() {
     console.log('>>openRef()...');
 
-    const navUsers = document.getElementById("nav-users");
-
     const $dashboard = document.getElementById("dashboard");
-    $dashboard.innerHTML = "It's work!";
 
+    //$dashboard.innerHTML = '<div type="text/html" data="{% include "./template/common/references.twig" %}"></div>';       
+    $dashboard.innerHTML = ">It's work!";
+    //$dashboard.innerHTML = '<div>{% include "./template/common/references.twig" %}</div>';
+   
+    //$dashboard.innerHTML = '<button type="button" class="btn btn-primary d-flex gap-2 align-items-center" data-bs-toggle="modal" data-bs-target="#refModal">'
+    //                     +'<i class="fa fa-plus-square"></i>'
+    //                     +'<span>{{ button_add }}</span>'
+    //                 +'</button>';    
 }
-
-//const Counterpartie = document.getElementById("Counterpartie");
-//Counterpartie.addEventListener("click", await openRef());
-
 //Content////////////////////////////////////////////////////////////////////
 async function navItem(navTab, name) {
     const li = document.createElement('li');
@@ -45,6 +46,19 @@ async function navItem(navTab, name) {
         a.innerText = name;        
     li.appendChild(a); 
     navTab.appendChild(li); 
+}
+async function dashboardItem(node, name) {
+    const li = document.createElement('li');
+    li.setAttribute("class","nav-item");
+        const a = document.createElement('a');
+        a.setAttribute("class","nav-link");        
+        a.setAttribute("id", name);           
+        a.innerText = name;
+        a.setAttribute("style","color:grey;font-size: 19px;");       
+        a.setAttribute("onclick", "openRef()");     
+    li.appendChild(a); 
+    node.appendChild(li); 
+    return node;
 }
 async function header(navTab) {
     console.log('>>header()...');
@@ -63,13 +77,13 @@ async function header(navTab) {
 async function dashboard(div) {
     console.log('>>dashboard()...');
 
-    const div2 = document.createElement('div');
-    div2.setAttribute("class", "tab-content p-4 gap-2");          
-        const div4 = document.createElement('div');
-        div4.setAttribute("class", "tab-pane fade");   
-        div4.setAttribute("role", "tabpanel");          
-        div2.appendChild(div4);  
-    div.appendChild(div2);  
+    // const div2 = document.createElement('div');
+    // div2.setAttribute("class", "tab-content p-4 gap-2");          
+    //     const div4 = document.createElement('div');
+    //     div4.setAttribute("class", "tab-pane fade");   
+    //     div4.setAttribute("role", "tabpanel");          
+    //     div2.appendChild(div4);  
+    // div.appendChild(div2);  
 
     let data = await getConfig();
     for (let row of data) {
@@ -78,12 +92,11 @@ async function dashboard(div) {
         if (!row.state===0||elements.typeId==='Constant'||elements.typeId==='Subsystem') {
             console.log(elements.typeId);
         } else {    
-            const a = document.createElement('a');
-            a.setAttribute("class","nav-link");  
-            a.setAttribute("style","color:grey;font-size: 19px;");  
-            a.innerText = elements.textId;
-            a.setAttribute("onclick", "openRef()");
-            div2.appendChild(a);  
+            const ul = document.createElement('ul');
+            ul.setAttribute("class","nav navbar-nav gap-2");  
+            ul.setAttribute("role","tablist");                 
+                dashboardItem(ul, elements.textId);                    
+            div.appendChild(ul);  
         }
     }        
 }
@@ -98,12 +111,11 @@ function init() {
         const navTab = document.getElementById("eva-nav");
         header(navTab);
         const div = document.createElement('div');   
-        div.setAttribute("class", "d-flex flex-row flex-grow-1");                          
-            const div3 = document.createElement('div');
-            div3.setAttribute("id","dashboard");
-            div3.setAttribute("class","col");
-            div3.setAttribute("style", "height:calc(100vh - 96px); border: 1px solid #00ff92");                
-            //div3.insertAdjacentHTML = '{% include "./template/common/dashboard.twig" %}';
+        div.setAttribute("class", "d-flex flex-row flex-grow-1");                                  
+        div.setAttribute("style", "height:calc(100vh - 96px); border: 1px solid #00ff92");       
+            const div3 = document.createElement('div');           
+            div3.setAttribute("class","col p-4 gap-2");
+            div3.setAttribute("id","dashboard");            
             dashboard(div3);
             div.appendChild(div3); 
         app.appendChild(div);

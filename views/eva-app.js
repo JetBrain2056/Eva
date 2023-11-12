89d2bb5c8716fbea9a1639751fb32eb449e70cb2
JetBrain2056 <st@brainrs.com>
2023-11-12 13:40:44 +0300

    Update eva-app.js

--- views/eva-app.js
+++ views/eva-app.js
@@ -1,98 +1,7 @@
 let a = [];
 selectRows = [];
 n = 0;
-//tbl = [];
-// async function showAppTable(showTbl, hide, col, data) {
-//     console.log('>>showTable()...'); 
-
-//     //console.log('table: ' + showTbl); 
-   
-//     if (showTbl) {
-//         showTbl.addEventListener('click', rowSelect);
-  
-//         showTbl.addEventListener('dblclick',  (e) => {
-          
-//           if (e.target.nodeName  === 'TH') {
-//             return;
-//           } else {
-//             const currentForm = e.currentTarget.parentNode.parentNode;
-              
-//             const modalTrigger = currentForm.getElementsByClassName('eva-edit');
-//             if (modalTrigger[0]) {
-//               modalTrigger[0].click();
-//             }
-//           }
-//         });
-//     } else {       
-//         return;
-//     }    
-  
-//     const thead = document.createElement('thead');
-//     thead.style.position = 'sticky';  
-//     thead.style.top      = '0px';
-//     thead.style.border   = '#00ff92';
-//     thead.style.background = 'White';  
-//     showTbl.appendChild(thead);
-  
-//     const tbody = document.createElement('tbody');
-//     showTbl.appendChild(tbody);
-  
-//     const tr = document.createElement('tr'); 
-//     thead.appendChild(tr);
-    
-//     for (const e of Object.keys(col)) {             
-//       const th = document.createElement('th');    
-//       th.setAttribute("sort-attr", "");                        
-//       for (const h of hide) {   
-//         if (e===h)     
-//         th.style.display = "none";        
-//       }
-//       tr.appendChild(th);        
-//       th.textContent = col[e];      
-//     }       
-  
-//     if (data) {
-//         for (const rows of data) {                  
-//             const tr = document.createElement('tr');
-//             tbody.appendChild(tr);             
-//             for (let p of Object.keys(col)) {            
-//                 const td = document.createElement('td');    
-//                 tr.appendChild(td);              
-//                 td.textContent = rows[p];    
-//                 for (const h of hide) {   
-//                     if (p===h)     
-//                     td.style.display = "none";        
-//                 }
-//             }
-//         } 
-//     } else {
-//         console.log('data: '+data)
-//         return;   
-//     }
-// }
-//Get on server//////////////////////////////////////////////////////////////
-// async function getSubsystems() {
-//     console.log('>>getSubsystems()...');
-//     let res;
-//     try{
-//         const response = await fetch('/subsystems');
-//         res = await response.json();
-//     } catch (err) {
-//         console.log(err)
-//     }
-//     return res;  
-// }
-// async function getConfig() {
-//     console.log('>>getConfig()...');
-//     let res;
-//     try{
-//         const response = await fetch('/config');
-//         res = await response.json();
-//     } catch (err) {
-//         console.log(err)
-//     }
-//     return res;
-// }
+//Get on Server//////////////////////////////////////////////////////////////
 async function getReferences(refName) {
     console.log('>>getReferences()...');
 
@@ -110,27 +19,6 @@ async function getReferences(refName) {
     } catch (err) {
       console.log(err);
     }
-}
-async function showRefTable(refName) {
-    console.log('>>showRefTable()...');
-
-    const refFormLabel = document.getElementById("refFormLabel"); 
-    refFormLabel.innerText = refName+'s';
-
-    const formTbl = document.getElementById("eva-ref-form");    
-    formTbl.innerHTML='';
-    formTbl.setAttribute("style", "height: calc(100vh - 171px); overflow-y: scroll;");               
-        const refTbl = document.createElement('table');
-        refTbl.setAttribute("class", "table table-striped table-hover table-sm table-responsive");              
-    formTbl.appendChild(refTbl);      
-
-    let data = await getReferences(refName);   
-
-    const col  = { 'id':'Id' };  
-    const hide = [];      
-
-    await showTable(refTbl, hide, col, data);
-
 }
 //Commands on client/////////////////////////////////////////////////////////
 function clickLink() {
@@ -167,6 +55,27 @@ async function openRef(refName) {
     status.value = ">It's work!";
 }
 //Content////////////////////////////////////////////////////////////////////
+async function showRefTable(refName) {
+    console.log('>>showRefTable()...');
+
+    const refFormLabel = document.getElementById("refFormLabel"); 
+    refFormLabel.innerText = refName+'s';
+
+    const formTbl = document.getElementById("eva-ref-form");    
+    formTbl.innerHTML='';
+    formTbl.setAttribute("style", "height: calc(100vh - 171px); overflow-y: scroll;");               
+        const refTbl = document.createElement('table');
+        refTbl.setAttribute("class", "table table-striped table-hover table-sm table-responsive");              
+    formTbl.appendChild(refTbl);      
+
+    let data = await getReferences(refName);   
+
+    const col  = { 'id':'Id' };  
+    const hide = [];      
+
+    await showTable(refTbl, hide, col, data);
+
+}
 function navItem(navTab, name) {    
     const li = document.createElement('li');
     li.setAttribute("class","nav-item");
@@ -190,23 +99,21 @@ function navItem(navTab, name) {
     navTab.appendChild(li);  
 
     const evaSubsys = document.querySelector('.eva-subsys'); 
-
-    if (name=='Desktop'||name=='References'||name=='Reports') {
-        //main
-    } else {
-        //subsystem
-        const subsys = document.createElement("div");        
-        subsys.setAttribute("class","tab-pane fade");    
-        subsys.setAttribute("role","tabpanel");
-        subsys.setAttribute("id", "nav-"+name);
-        subsys.setAttribute("aria-labelledby","nav-"+name+"-tab");
-            const h5 = document.createElement("h5");  
-            h5.setAttribute("class","title");
-            h5.innerText = name;
-            h5.appendChild(document.createElement("hr"));  
-        subsys.appendChild(h5);    
-        evaSubsys.appendChild(subsys);            
-    }    
+    const subsys = document.createElement("div"); 
+    if (name=='Desktop') {    
+        subsys.setAttribute("class","tab-pane fade active show");      
+    } else {                       
+        subsys.setAttribute("class","tab-pane fade");   
+    }
+    subsys.setAttribute("role","tabpanel");
+    subsys.setAttribute("id", "nav-"+name);
+    subsys.setAttribute("aria-labelledby","nav-"+name+"-tab");
+        const h5 = document.createElement("h5");  
+        h5.setAttribute("class","title");
+        h5.innerText = name;
+        h5.appendChild(document.createElement("hr"));  
+    subsys.appendChild(h5);    
+    evaSubsys.appendChild(subsys);                   
 }
 function navLink(nav, name) {
     console.log('>>navLink()...');
@@ -223,15 +130,9 @@ async function header(navTab) {
     console.log('>>header()...');
     
     //MAIN
-    navItem(navTab, 'Desktop');
-    let div = document.getElementById("nav-Desktop");
-    tabDesk(div);    
-    navItem(navTab, 'References');    
-    div = document.getElementById("nav-References");
-    tabRef(div);    
+    navItem(navTab, 'Desktop'); 
+    navItem(navTab, 'References');         
     navItem(navTab, 'Reports');    
-    div = document.getElementById("nav-Reports");
-    //tabRep(div);   
 
     //DYNAMIC    
     let data = await getSubsystems();
@@ -240,6 +141,13 @@ async function header(navTab) {
         //console.log(row.name);
         navItem(navTab, row.name);           
     }  
+
+    let div = document.getElementById("nav-Desktop");
+    tabDesk(div);  
+    div = document.getElementById("nav-References");
+    tabRef(div);  
+    div = document.getElementById("nav-Reports");
+    //tabRep(div);    
 }
 async function tabDesk(div) {
     console.log('>>tabDesk()...');

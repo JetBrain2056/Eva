a2fcebe2b3b48b6d6f883cc27a5db3796791fb4a
JetBrain2056 <st@brainrs.com>
2023-11-14 08:58:46 +0300

    Update router.js

--- src/routers/router.js
+++ src/routers/router.js
@@ -10,7 +10,6 @@ router.get('/users', controller.getUsers);
 router.post('/getuser', controller.getUser);
 router.post('/createuser', controller.createUser);
 router.post('/updateuser', controller.updateUser);
-//router.delete('/delete/:id([0-9]+)', controller.delete)
 router.post('/deluser', controller.deleteUser);
 
 router.get('/roles', controller.getRoles);
@@ -22,7 +21,6 @@ router.post('/createconf', controller.createConfig);
 router.post('/delconf', controller.deleteConfig);
 router.post('/editobject', controller.editObject);
 router.post('/getobject', controller.getObject);
-
 router.post('/updateconf', controller.updateConfig);
 
 //eva-app
@@ -30,6 +28,7 @@ router.get('/subsystems', controller.getSubsystems);
 router.post('/getrefs', controller.getReferences);
 router.post('/getref', controller.getReference);
 router.post('/createref', controller.createReference);
+router.post('/updateref', controller.updateReference);
 router.post('/delref', controller.deleteReference);
 
 module.exports = { router }                                                  

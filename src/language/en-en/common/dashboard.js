8f8ee9ff0408de66a4c521285d5237c58883397f
JetBrain2056 <st@brainrs.com>
2024-02-23 11:25:08 +0300

    Update dashboard.js

--- src/language/en-en/common/dashboard.js
+++ src/language/en-en/common/dashboard.js
@@ -1,20 +1,23 @@
 let dashboard = {
-    // Heading
-    $_heading_title : 'Heading title',
-
-    // Error
-    $_error_install : 'Error: install',
-
-    //modal
-    $_text_add_user  : 'Add user',
+      
     $_text_subsystem : 'Subsystem',
     $_text_module    : 'Module',
     $_text_constant  : 'Constant',
     $_text_reference : 'Reference',
     $_text_document  : 'Document',
-    $_text_processing : 'Processing',
+    $_text_processing: 'Processing',
     $_text_register  : 'Register',
-    $_text_report    : 'Report'
+    $_text_report    : 'Report',
+
+    text_add_user    : 'Add an user:',
+    text_edit_user   : 'Edit an user:',
+    text_add_role    : 'Add an role:',    
+    text_add_object  : 'Add an object:',
+    text_edit_object : 'Edit an object:',
+    text_main        : 'Main',
+    text_requisites  : 'Requisites',
+    text_tabparts    : 'Tabular parts',
+    text_forms       : 'Forms'
 }
 
 module.exports = { dashboard }

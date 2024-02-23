c1ae96c7b15e149f86b86e340bde3ade58cd6f25
JetBrain2056 <st@brainrs.com>
2024-02-23 13:40:10 +0300

    Update dashboard.js

--- src/language/ru-ru/common/dashboard.js
+++ src/language/ru-ru/common/dashboard.js
@@ -5,7 +5,7 @@ let dashboard = {
     $_text_constant  : 'Константа',
     $_text_reference : 'Справочник',
     $_text_document  : 'Документ',
-    $_text_processing : 'Обработка',
+    $_text_processing: 'Обработка',
     $_text_register  : 'Регистр',
     $_text_report    : 'Отчет',
 
@@ -17,7 +17,18 @@ let dashboard = {
     text_main        : 'Основное',
     text_requisites  : 'Реквизиты',
     text_tabparts    : 'Табличные части',
-    text_forms       : 'Формы'
+    text_forms       : 'Формы',
+
+    text_name        : 'Name:',
+    text_descr       : 'Descr:',
+    text_role        : 'Role:',
+    text_email       : 'E-mail:',
+    text_password    : 'Password:',
+    text_cofirmpass  : 'Confirm password:',
+    text_show        : 'Show user:',
+    text_eauth       : 'Enterprise auth:',
+
+    text_select_roles: 'Select roles'
 }
 
 module.exports =   { dashboard }

f7f15f0ef506e36fb7af4f9520fac3e03ea4c6f7
JetBrain2056 <st@brainrs.com>
2024-02-23 11:25:12 +0300

    Update dashboard.js

--- src/language/ru-ru/common/dashboard.js
+++ src/language/ru-ru/common/dashboard.js
@@ -1,12 +1,5 @@
 let dashboard = {
-    // Heading
-    $_heading_title : 'Панель состояния',
-
-    // Error
-    $_error_install : 'ВНИМАНИЕ: ошибка установки приложения!',
-
-    //modal
-    $_text_addusers  : 'Добавить пользователя',
+    
     $_text_subsystem : 'Подсистема',
     $_text_module    : 'Модуль',
     $_text_constant  : 'Константа',
@@ -14,7 +7,17 @@ let dashboard = {
     $_text_document  : 'Документ',
     $_text_processing : 'Обработка',
     $_text_register  : 'Регистр',
-    $_text_report    : 'Отчет'
+    $_text_report    : 'Отчет',
+
+    text_add_user    : 'Добавить пользователя:',
+    text_edit_user   : 'Редактировать пользователя:',
+    text_add_role    : 'Добавить роль:',    
+    text_add_object  : 'Добавить объект:',
+    text_edit_object : 'Редактировать объект:',
+    text_main        : 'Основное',
+    text_requisites  : 'Реквизиты',
+    text_tabparts    : 'Табличные части',
+    text_forms       : 'Формы'
 }
 
 module.exports =   { dashboard }
